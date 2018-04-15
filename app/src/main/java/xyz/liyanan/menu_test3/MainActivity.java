afec5ba5583d3af625b4a1253bef869c1abf4fa3
superNN <liiyn@foxmail.com>
2018-04-15 15:50:21 +0800

    多级菜单的实现

--- app/src/main/java/xyz/liyanan/menu_test3/MainActivity.java
+++ app/src/main/java/xyz/liyanan/menu_test3/MainActivity.java
@@ -13,30 +13,39 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+    //普通菜单
+//    @Override
+//    public boolean onCreateOptionsMenu(Menu menu) {
+//        //获取MenuInflater
+//        MenuInflater inflater=getMenuInflater();
+//        //加载menu资源
+//        inflater.inflate(R.menu.oridinary,menu);
+//        //返回值必须为true，不然菜单无法显示
+//        return true;
+//    }
+//
+//    @Override
+//    public boolean onOptionsItemSelected(MenuItem item) {
+//        switch (item.getItemId()){
+//            case R.id.option1:
+//                return true;
+//            case R.id.option2:
+//                return true;
+//            case R.id.option3:
+//                return true;
+//            case R.id.option4:
+//                return true;
+//            default:
+//                return super.onOptionsItemSelected(item);
+//        }
+//    }
 
-    @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         //获取MenuInflater
-        MenuInflater inflater=getMenuInflater();
+        MenuInflater inflater = getMenuInflater();
         //加载menu资源
-        inflater.inflate(R.menu.oridinary,menu);
+        inflater.inflate(R.menu.multi, menu);
         //返回值必须为true，不然菜单无法显示
         return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        switch (item.getItemId()){
-            case R.id.option1:
-                return true;
-            case R.id.option2:
-                return true;
-            case R.id.option3:
-                return true;
-            case R.id.option4:
-                return true;
-            default:
-                return super.onOptionsItemSelected(item);
-        }
     }
 }
